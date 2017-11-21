9eed617afe2da74141090702cd864f50c3e875ed
Geoffrey Glangine <geoffrey.glangine@gmail.com>
2017-11-21 15:24:05 -0500

    finished paser

--- parser.py
+++ parser.py
@@ -12,6 +12,7 @@ urls=["http://paizo.com/pathfinderRPG/prd/bestiary/monsterIndex.html",
         "http://paizo.com/pathfinderRPG/prd/bestiary5/index.html"]
 number=0
 error=0
+dataCreature=[]
 for urlparse in urls:
     print(urlparse)
     page = urlopen(urlparse) # Insert your URL to extract
@@ -40,25 +41,35 @@ for urlparse in urls:
                 else:
                     index=0
                 try:
-                    print("#########################")
-                    print(name[index])
-                    print("#########################")
-                    if(len(name)>=1):
-                        if(index==len(name)-1):
-                            excerpts = re.findall(r'('+name[index]+')(.*?)('+'</div>'+')', newPage.text, re.DOTALL)
-                        else:
-                            excerpts = re.findall(r'('+name[index]+')(.*?)('+name[index+1]+'.*?</h1>)', newPage.text, re.DOTALL)
-                        for e in excerpts:
-                            soup2 = BeautifulSoup(''.join(e),"lxml")
-                            for link2 in soup2.findAll('a'):
-                                if(str(link2).find('/spells/')==-1):
-                                    continue
-                                    
-                                else :
-                                    print(link2.next)
+                    #print("#########################")
+                    #print(name[index])
+                    spells=[]
+                    #print("#########################")
+                    if(index==len(name)-1):
+                        excerpts = re.findall(r'('+name[index]+')(.*?)('+'</div>'+')', newPage.text, re.DOTALL)
+                    else:
+                        excerpts = re.findall(r'('+name[index]+')(.*?)('+name[index+1]+'.*?</h1>)', newPage.text, re.DOTALL)
+                    for e in excerpts:
+                        soup2 = BeautifulSoup(''.join(e),"lxml")
+                        for link2 in soup2.findAll('a'):
+                            if(str(link2).find('/spells/')==-1):
+                                continue
+                                
+                            else :
+                                p = re.compile(r'<.*?>')
+                                if(p.sub('',link2.next)!="Spells" or p.sub('',link2.next)!="Spell Lists" or p.sub('',link2.next)!="Spells"):
+                                    spells.append(p.sub('',link2.next))
+                    
+                    creature = ({"name":name[index],
+                        "spells":spells})
+                    dataCreature.append(creature)
                 except:
                     error+=1
                 namePrevious=name
                 number+=1
+                print(number)
 print(number)
 print(error)
+with open('data.json', 'w') as outfile:
+    json.dump(dataCreature, outfile)
+print(dataCreature)
