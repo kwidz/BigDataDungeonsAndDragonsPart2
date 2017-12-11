02fcad46ecc5a47a0616f088f6b68de8467608a7
morgane <mcabrol21@gmail.com>
2017-12-11 13:44:10 -0500

    Creation Graph 2

--- Scala/src/Exercice2Combat2.scala
+++ Scala/src/Exercice2Combat2.scala
@@ -7,11 +7,20 @@ import org.apache.spark.{SparkContext, SparkConf}
  */
 object Exercice2Combat2 {
   def main(args: Array[String]): Unit = {
+    var graph: Graph[(Creature, Position), Long] = null
+
+    def calculateDistance(positionA: Position, positionB: Position ) : Long = {
+      return math.round(math.sqrt(math.pow(positionA.x-positionB.x,2)+math.pow(positionA.y-positionB.y,2)+math.pow(positionA.z-positionB.z,2)))
+
+    }
 
     val conf = new SparkConf().setAppName("Simple Application").setMaster("local[*]")
     val sc = new SparkContext(conf)
     sc.setLogLevel("ERROR")
-    var array = new Array[(VertexId,(Creature, Position))](221)
+    var arrayRDD = new Array[(VertexId,(Creature, Position))](221)
+    var arrayDesMechants = new Array[VertexId](211)
+    var arrayDesGentils = new Array[VertexId](10)
+    var arrayEdges = new Array[Edge[Long]](2110)
 
 
 
@@ -23,35 +32,49 @@ object Exercice2Combat2 {
     var i = 0
 
     //Add solar
-    array.update(i,(i,(new Solar(),new Position(x,y,0))))
+    arrayRDD.update(i,(i,(new Solar(),new Position(x,y,0))))
+    arrayDesGentils.update(i,i)
 
     // Add 2 planetar et 2 movanic deva derriere solar
     i+=1
-    array.update(i,(i,(new Planetar(),new Position(-10,10,0))))
+    arrayRDD.update(i,(i,(new Planetar(),new Position(-10,10,0))))
+    arrayDesGentils.update(i,i)
     i+=1
-    array.update(i,(i,(new MovanicDeva(),new Position(-10,20,0))))
+    arrayRDD.update(i,(i,(new MovanicDeva(),new Position(-10,20,0))))
+    arrayDesGentils.update(i,i)
     i+=1
-    array.update(i,(i,(new Planetar(),new Position(-10,-10,0))))
+    arrayRDD.update(i,(i,(new Planetar(),new Position(-10,-10,0))))
+    arrayDesGentils.update(i,i)
     i+=1
-    array.update(i,(i,(new MovanicDeva(),new Position(-10,-20,0))))
+    arrayRDD.update(i,(i,(new MovanicDeva(),new Position(-10,-20,0))))
+    arrayDesGentils.update(i,i)
 
     //Add 5 astral deva
     i+=1
-    array.update(i,(i,(new AstralDeva(),new Position(-20,0,0))))
+    arrayRDD.update(i,(i,(new AstralDeva(),new Position(-20,0,0))))
+    arrayDesGentils.update(i,i)
     i+=1
-    array.update(i,(i,(new AstralDeva(),new Position(-20,10,0))))
+    arrayRDD.update(i,(i,(new AstralDeva(),new Position(-20,10,0))))
+    arrayDesGentils.update(i,i)
     i+=1
-    array.update(i,(i,(new AstralDeva(),new Position(-20,20,0))))
+    arrayRDD.update(i,(i,(new AstralDeva(),new Position(-20,20,0))))
+    arrayDesGentils.update(i,i)
     i+=1
-    array.update(i,(i,(new AstralDeva(),new Position(-20,-10,0))))
+    arrayRDD.update(i,(i,(new AstralDeva(),new Position(-20,-10,0))))
+    arrayDesGentils.update(i,i)
     i+=1
-    array.update(i,(i,(new AstralDeva(),new Position(-20,-20,0))))
+    arrayRDD.update(i,(i,(new AstralDeva(),new Position(-20,-20,0))))
+    arrayDesGentils.update(i,i)
 
+
+var j = 0
     //Add 200 barbar orc
     for(x<- 100 until 200 by 10){
       for(y <- 100 until -100 by -10 ){
         i+=1
-        array.update(i,(i,(new BarbareOrc(),new Position(x,y,0))))
+        arrayRDD.update(i,(i,(new BarbareOrc(),new Position(x,y,0))))
+        arrayDesMechants.update(j,i)
+        j += 1
       }
      xsave = x
     }
@@ -60,16 +83,39 @@ object Exercice2Combat2 {
     x = xsave + 10
     for(y <- 50 to -40 by -10 ){
       i+=1
-      array.update(i,(i,(new AngelSlayer(),new Position(x,y,0))))
+
+      arrayRDD.update(i,(i,(new AngelSlayer(),new Position(x,y,0))))
+      arrayDesMechants.update(j,i)
+      j+=1
     }
 
     // add Green Great Wyrm Dragon
     x = x + 10
     i+=1
-    array.update(i,(i,(new DragonGreen(),new Position(x,0,0))))
+    arrayRDD.update(i,(i,(new DragonGreen(),new Position(x,0,0))))
+    arrayDesMechants.update(j,i)
 
 
-    val creatureRDD: RDD[(VertexId,(Creature, Position))] =
-      sc.parallelize(array)
+    val creatureRDD: RDD[(VertexId,(Creature, Position))] =  sc.parallelize(arrayRDD)
+
+    var u,v,t = 0
+    for(u <- 0 until arrayDesGentils.length){
+      for(v <- 0 until arrayDesMechants.length){
+        arrayEdges.update(t,Edge(u, v, calculateDistance(creatureRDD.lookup(v).head._2,creatureRDD.lookup(u).head._2)))
+        t +=1
+
+      }
+    }
+
+
+    val edges : RDD[Edge[Long]] = sc.parallelize(arrayEdges)
+
+    graph = Graph(creatureRDD, edges)
+
+
+
+
   }
+
+
 }
